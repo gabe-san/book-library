de31f6d90391c1dd23b74627b7da07e275998588
gabe-san <gsantosx98@gmail.com>
2023-06-10 19:30:43 -0700

    fixed 2 functions

--- main.js
+++ main.js
@@ -7,14 +7,33 @@ function Books(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    console.log(`${title} by ${author}, ${pages} pages, ${read}`);
-  };
+  this.info = () => `${title} by ${author}, ${pages} pages, ${read}`;
 }
-const theHobbit = new Books('The Hobbit', 'J.R.R Tolkien', '295', 'not read yet');
+
+// manually add one book to test
+const theHobbit = new Books(
+  "The Hobbit",
+  "J.R.R Tolkien",
+  "295",
+  "not read yet"
+);
 console.log(theHobbit.info());
+// for user input, you must getElement or querySelector each from the form in order to implement new Book.
 
 // function to add Books to myLibrary
-function addBookToLibrary() {
-  
+function addBookToLibrary(bookConstructor) {
+  // .push() object from constructor to myLibrary. bookAdd returns length of new array(myLibrary)
+
+  const bookAdd = myLibrary.push(bookConstructor);
+  // newObjectFromBookConstructor.push(myLibrary);
+  // console.log(myLibrary[bookAdd]);
 }
+addBookToLibrary(theHobbit);
+function displayLibrary() {
+  return myLibrary;
+  // for loop myLibrary
+  // if object exists myLibrary, change textContent of html card on page
+}
+
+// some useful methods for project: arr.find(function(item,index,array))
+// https://javascript.info/array-methods
