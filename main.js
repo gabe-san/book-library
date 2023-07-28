05467c58129c27cc6a28b2b1e6e88f3fe14f2deb
gabe-san <gsantosx98@gmail.com>
2023-07-28 14:35:55 -0700

    Edit forms and removed comments

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ function Books(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.isRead = false; // to do: make conditional Example: let this.isRead = (read = true) ? true : false
   this.info = () => `${title} by ${author}, ${pages} pages, ${read}`;
 }
+// search how to make booleans in object constructor
 
 // manually add one book to test
 const theHobbit = new Books(
@@ -17,23 +18,28 @@ const theHobbit = new Books(
   "295",
   "not read yet"
 );
-console.log(theHobbit.info());
+
 // for user input, you must getElement or querySelector each from the form in order to implement new Book.
 
 // function to add Books to myLibrary
 function addBookToLibrary(bookConstructor) {
-  // .push() object from constructor to myLibrary. bookAdd returns length of new array(myLibrary)
-
   const bookAdd = myLibrary.push(bookConstructor);
-  // newObjectFromBookConstructor.push(myLibrary);
-  // console.log(myLibrary[bookAdd]);
+
+  return bookAdd;
 }
 addBookToLibrary(theHobbit);
+
 function displayLibrary() {
   return myLibrary;
   // for loop myLibrary
   // if object exists myLibrary, change textContent of html card on page
 }
 
-// some useful methods for project: arr.find(function(item,index,array))
-// https://javascript.info/array-methods
+/* work flow:
+1: store user input into variables. form or prompt?
+2: find a way to connect those variables into the Book Constructor
+3: use addBookToLibrary to store the newly created object in myLibrary[]
+4: ***Test*** console.log to see if array properly stores it
+5: displayLibrary() to automatically fill new html divs to showcase object contents of myLibrary();
+6: Add QoL button inputs: Remove book, add new book, Read status
+*/
