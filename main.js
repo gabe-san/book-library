1c068227a643bfa6d02b3ad9382dfb645c86394d
gabe-san <gsantosx98@gmail.com>
2023-07-29 17:27:08 -0700

    fixed addBooktoLibrary function and added show/submit event listeners

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
-// Store Book objects within array
 const myLibrary = [];
 
-// Write a constructor for making "Book" objects
 function Books(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.isRead = false; // to do: make conditional Example: let this.isRead = (read = true) ? true : false
+  this.isRead = read;
   this.info = () => `${title} by ${author}, ${pages} pages, ${read}`;
 }
-// search how to make booleans in object constructor
-
-// manually add one book to test
-const theHobbit = new Books(
-  "The Hobbit",
-  "J.R.R Tolkien",
-  "295",
-  "not read yet"
-);
-
-// for user input, you must getElement or querySelector each from the form in order to implement new Book.
 
 // function to add Books to myLibrary
-function addBookToLibrary(bookConstructor) {
-  const bookAdd = myLibrary.push(bookConstructor);
-
-  return bookAdd;
+function addBookToLibrary() {
+  const title = document.getElementById("book-title").value;
+  const author = document.getElementById("author-name").value;
+  const pages = document.getElementById("page-count").value;
+  const read = document.getElementById("user-readstatus").checked;
+  const newBook = new Books(title, author, pages, read);
+  console.log(newBook);
 }
-addBookToLibrary(theHobbit);
+
+// show form
+const addBookBtn = document.querySelector("#addBook");
+addBookBtn.addEventListener("click", () => {
+  const newBookForm = document.querySelector("#newBookForm");
+  newBookForm.style.display = "block";
+});
+// submit form
+const submitBookBtn = document.querySelector("#updateLibraryBtn");
+submitBookBtn.addEventListener("submit", () => {
+  alert("hello"); // event prevent default probably why it doesnt show
+});
 
 function displayLibrary() {
   return myLibrary;
@@ -36,10 +37,10 @@ function displayLibrary() {
 }
 
 /* work flow:
-1: store user input into variables. form or prompt?
+1: store user input into variables. form 
 2: find a way to connect those variables into the Book Constructor
 3: use addBookToLibrary to store the newly created object in myLibrary[]
 4: ***Test*** console.log to see if array properly stores it
 5: displayLibrary() to automatically fill new html divs to showcase object contents of myLibrary();
-6: Add QoL button inputs: Remove book, add new book, Read status
+
 */
